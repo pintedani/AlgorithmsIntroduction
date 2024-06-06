e6f27c82e15685d5d09834b47955061343387f39
Pinte Dan Mircea <pintedani@yahoo.com>
2024-06-06 11:37:25 +0300

    Update value_counting_with_dictionary.py

--- AlgorithmsIntroduction/Finished/value_counting_with_dictionary.py
+++ AlgorithmsIntroduction/Finished/value_counting_with_dictionary.py
@@ -1,24 +1,14 @@
 def count_values(list):
-    #Implement method
-    return 0
+    counter = dict()
+    for item in list:
+        if item in counter.keys():
+            counter[item] +=1
+        else:
+            counter[item] = 1
+    return counter
 
 # Usage example
 color_list = ["red", "blue", "red", "green", "blue", "red", "red"]
 
 color_counts = count_values(color_list)
 print("Value counts: ", color_counts)
-
-#------------------
-
-
-items = ["red", "blue", "red", "green", "blue", "red", "red"]
-
-counter = dict()
-
-for item in items:
-    if item in counter.keys():
-        counter[item] += 1
-    else:
-        counter[item] = 1
-        
-print(counter)
